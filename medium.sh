14766c89544b3c37b0dabc4c03f26bf43f3a4c92
nctamer <nazifcan.tamer@upf.edu>
2021-06-14 19:53:06 +0200

    Change into dict dataset

--- medium.sh
+++ medium.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=crepe_med
 #SBATCH -n 16
 #SBATCH --time=8:00:00
-#SBATCH --mem 50000
+#SBATCH --mem 72000
 #SBATCH --partition=medium
 #SBATCH --gres=gpu:1
 #SBATCH -p medium                     # Partition to submit to
